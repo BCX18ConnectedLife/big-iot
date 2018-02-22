7b607238a971dcfe6ebc785d3375de59793f0451
Stefan Schmid <30600130+schmid-bosch@users.noreply.github.com>
2018-02-22 09:15:06 +0100

    Update nodejs-provider.js

--- nodejs-provider.js
+++ nodejs-provider.js
@@ -1,61 +1,61 @@
-const { provider: BigIotProvider, offering: BigIotOffering } = require('bigiot-js');
-
-// Configure your provider by setting the BIGIOT_PROVIDER_ID and BIGIOT_PROVIDER_SECRET environment
-// variables
-const config = {
-  id: process.env.BIGIOT_PROVIDER_ID,
-  secret: process.env.BIGIOT_PROVIDER_SECRET,
-};
-
-const provider = new BigIotProvider(config.id, config.secret);
+const bigiot = require('bigiot-js');
+const provider = new bigiot.provider("... Your Provider ID ...", "... Your Provider Secret ...");
 
 function createOffering() {
-  // Instantiate an offering of the desired type
-  const offering = new BigIotOffering('TestOffering', 'urn:proposed:Miscellaneous');
 
-  // Define the HTTP endpoint consumers should call on your service
-  offering.endpoints = {
-    uri: 'http://example.net/some/path',
-  };
+        // Instantiate an offering of the desired type
+        const offering = new bigiot.offering("TestOffering", "urn:proposed:Miscellaneous");
 
-  // Define the input parameters your offering accepts, if any
-  offering.inputData = [
-    {
-      name: 'latitude',
-      rdfUri: 'http://schema.org/latitude',
-    },
-    {
-      name: 'longitude',
-      rdfUri: 'http://schema.org/longitude',
-    },
-  ];
+        // Define the HTTP endpoint consumers should call on your service
+        offering.endpoints = {
+          uri: 'http://example.net/some/path',
+        };
 
-  // Define the data structure your offering returns when called
-  offering.outputData = [
-    {
-      name: 'temperature',
-      rdfUri: 'http://schema.org/airTemperatureValue',
-    },
-  ];
-  
-  offering.price = {
-    pricingModel: 'PER_ACCESS',
-    money: { 
-       amount: 0.001,
-       currency: 'EUR',
-    },
-  };
-  
-  return offering;
+        // Define the geographical extent of your offering
+        offering.extent = {
+          city: 'Station, Berlin',
+        };
+
+        // Define the input parameters your offering accepts, if any
+        offering.inputData = [
+          {
+            name: 'latitude',
+            rdfUri: 'http://schema.org/latitude',
+          },
+          {
+            name: 'longitude',
+            rdfUri: 'http://schema.org/longitude',
+          },
+        ]
+
+        // Define the data structure your offering returns when called
+        offering.outputData = [
+          {
+            name: "temperature",
+            rdfUri: 'http://schema.org/airTemperatureValue',
+          }
+        ];
+
+        offering.price = {
+                pricingModel: 'PER_ACCESS',
+                money: {
+                        amount: 0.001,
+                        currency: 'EUR',
+                },
+        };
+
+        return offering;
 }
 
 provider.authenticate()
   .then(createOffering)
-  .then((offering) => {
-    provider.register(offering)
-      .then(() => {
-        console.log(`Offering ${offering.id} registered`);
-        // Re-register offering periodically to keep it active
-        setInterval(() => provider.register(offering), 30000);
-      });
-  });
+        .then((offering) => {
+                provider.register(offering)
+                        .then(() => {
+                                console.log(`Offering ${offering.id} registered`);
+                                const interval = setInterval(() => provider.register(offering), 30000);
+                                // clearInterval(interval);
+                        });
+        });
+
+
